f4ed8bf7fc7492c6edb048bb4da72d0cde1741e0
Luka <luka@dimjasevic.net>
2023-06-19 15:46:45 +0200

    Removing unused components

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,16 +1,10 @@
 import Navbar from "./Navbar";
-import PrimaryButton from "./PrimaryButton";
-import Input from "./Input";
 import Loader from "./Loader";
-import Tip from "./Tip";
 import Sidebar from "./Sidebar";
 
 
 export {
 	Navbar,
-	PrimaryButton,
-	Input,
 	Loader,
-	Tip,
 	Sidebar
 };
